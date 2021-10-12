985c1769e3aedef7d12bde94261691cfe6871822
Amanda <mandathompson@yahoo.com>
2021-10-12 12:33:59 -0400

    dependencies, configuring render

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default App = () => {
+    return(
+        <h1>React App</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
