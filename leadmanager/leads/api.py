da21ba14044446e686cf8c51252a4e4309d5e3c6
Amanda <mandathompson@yahoo.com>
2021-10-12 10:02:26 -0400

    first viewset

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -2,3 +2,10 @@ from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer 
 
+# Lead Viewset 
+class LeadViewSet(viewsets.ModelViewset):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.Allowany
+    ]
+    serializer_class = LeadSerializer
--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from leads.models import Lead
 
+# Lead Serializer 
 class LeadSerializer(serializers.Serializer):
     class Meta:
         model = Lead
-        fields = '__all__'
+        fields = '__all__' 
+
